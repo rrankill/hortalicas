c62520cafb2426abe88646b5a2b3fcebebe6ed2b
Marcelo <marceloguilherme2020@gmail.com>
2026-08-14 20:54:06 -0300

    colocando a página home

--- hortalicas/src/components/Cardhortalicas.jsx
+++ hortalicas/src/components/Cardhortalicas.jsx
@@ -1,9 +1,9 @@
-function CardHortalicas({ titulo, descriçao, imagem }) {
+function CardHortalicas({ titulo, propaganda, imagem }) {
     return (
     <div className="card-hortalicas">
         <img src={imagem} alt={`Imagem da hortalica ${titulo}`} className="card-imagem" />
         <h3 className="card-titulo">{titulo}</h3>
-        <p className="card-descriçao">{descriçao}</p>
+        <p className="card-propaganda">{propaganda}</p>
     </div>
   );
 }
