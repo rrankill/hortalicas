6d35b449967ac8ed9792930cf90b2f6e530e920c
marcosdev2007 <marcoscristiano787@gmail.com>
2026-08-14 19:15:26 -0300

    adicionando dados do sistema

---
+++ hortalicas/src/data/hortalicas.js
@@ -0,0 +1,43 @@
+//src/data/hortalicas.js
+export const hortalicas =[
+    {
+        id:1,
+        titulo:"alface",
+        propaganda:"alface fresca e crocante, perfeita para saladas,",
+        imagem:"https://example.com/alface.jpg"
+
+    },
+    {
+        id:2,
+        titulo:"tomate",
+        propaganda:"tomates maduros e suculentos, ideais para molhos e saladas,",
+        imagem:"https://example.com/tomate.jpg"
+    },
+    {
+        id:3,
+        titulo:"cenoura",
+        propaganda:"cenouras doces e nutritivas, ótimas para lanches e receitas,",
+        imagem:"https://example.com/cenoura.jpg"
+     
+    },
+    {
+        id:4,
+        titulo:"pepino",
+        propaganda:"pepinos frescos e crocantes, perfeitos para saladas e conservas,",
+        imagem:"https://example.com/pepino.jpg"
+    },
+    {
+        id:5,
+        titulo:"brócolis",
+        propaganda:"brócolis frescos e nutritivos, ideais para saladas e pratos quentes,",
+        imagem:"https://example.com/brócolis.jpg"
+    },
+    {
+        id:6,
+        titulo:"pimentão",
+        propaganda:"pimentões coloridos e saborosos, ótimos para receitas e saladas,",
+        imagem:"https://example.com/pimentão.jpg"
+    },
+    
+    
+];
